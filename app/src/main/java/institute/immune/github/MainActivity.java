24986ed88ecc672afbc33a7121eb7863d17a8f57
diegoperezceada <55735195+diegoperezceada@users.noreply.github.com>
2022-02-01 10:48:04 +0100

    Update MainActivity.java
    he quitado el punto y coma

--- app/src/main/java/institute/immune/github/MainActivity.java
+++ app/src/main/java/institute/immune/github/MainActivity.java
@@ -2,7 +2,7 @@ package institute.immune.github;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import android.os.Bundle
 
 public class MainActivity extends AppCompatActivity {
 
