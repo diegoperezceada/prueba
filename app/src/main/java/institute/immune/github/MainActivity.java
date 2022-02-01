b5591edbc9f68665b8f368c27e54313aff3f8c46
diego <diego.perez.ceb@immune.institute>
2022-02-01 10:33:28 +0100

    initial

---
+++ app/src/main/java/institute/immune/github/MainActivity.java
@@ -0,0 +1,18 @@
+package institute.immune.github;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * APP DE PRUEBA PARA GITHUB
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
